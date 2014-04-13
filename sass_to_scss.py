2caf3614e2bdb26c63ca9de7f61e223431bfc2a7
Luqmaan <ldawoodjee@gmail.com>
2014-04-13 18:56:01 -0500

    Many changes and fixes
    Setup Travis CI
    Add requirements.txt
    Fix bug when importing urls
    Fix bugs and handle tests when using two spaces
    Fix bug when using psuedoelement selector that would cause it to be interpreted as css statement
    Remove all empty lines, as they are tricky
    Switch to a class

--- sass_to_scss.py
+++ sass_to_scss.py
@@ -1,73 +1,105 @@
 import argparse
-from pprint import pprint
 import re
+import logging
+
+DEFAULT_INDENTATION = 4
 
 
-def count_leading_spaces(line):
-    return len(line) - len(line.lstrip(' '))
+class SassToSCSS(object):
+
+    def __init__(self, loglevel=None, spaces=DEFAULT_INDENTATION):
+
+        self.spaces = spaces
+        self.logger = logging.getLogger()
+        self.logger.setLevel(loglevel or logging.WARNING)
+
+    @classmethod
+    def count_leading_spaces(cls, line):
+        return len(line) - len(line.lstrip(' '))
+
+    def convert(self, sass_lines):
+        outlines = []
+        cleaned = []
+
+        re_css_rule = re.compile(r'\s*[\w\-\d]+:')
+        re_import = re.compile(r'@import')
+        re_sass_mixin = re.compile(r'\=\w+')
+        re_sass_include = re.compile(r'\+\w+')
+        re_sass_variable = re.compile(r'\s*\$')
+
+        for line in sass_lines:
+            line = line.replace('\n', '')
+
+            if re_import.match(line):
+                if not '@import "' in line:
+                    line = line.replace('@import ', '@import "')
+                    line += '"'
+                line += ';'
+            elif re_sass_mixin.search(line):
+                line = line.replace('=', '@mixin ')
+            elif re_sass_include.search(line):
+                line = line.replace('+', '@include ')
+                line += ';'
+            elif re_css_rule.match(line):
+                line += ';'
+            elif re_sass_variable.match(line):
+                line += ';'
+
+            # an empty line resets the indent back to zero, skipping them solves that problem
+            if line == '':
+                continue
+
+            cleaned.append(line)
+
+        # add an extra element in case we get to the end and
+        # we need one more iteration to figure out the }
+        # because we need to get next_indent
+        cleaned.append('')
+
+        for i, line in enumerate(cleaned):
+            indent = self.count_leading_spaces(cleaned[i])
+            try:
+                next_indent = self.count_leading_spaces(cleaned[i + 1])
+            except IndexError:
+                next_indent = 0
+            try:
+                prev_indent = self.count_leading_spaces(cleaned[i - 1])
+            except IndexError:
+                prev_indent = 0
+
+            logging.debug('{} next_indent {}, indent {}, prev_indent {}, {}'.format(i, next_indent, indent, prev_indent, line))
+
+            if indent < next_indent:
+                line = line + ' ' + '{'
+                logging.debug('{')
+
+            if indent < prev_indent:
+                closed_indent = prev_indent
+                while closed_indent >= next_indent and closed_indent != 0:
+                    logging.debug('}} : {}-{}={}'.format(closed_indent, next_indent, closed_indent - next_indent))
+                    insert = ' ' * (closed_indent - self.spaces) + '}'
+                    outlines.append(insert)
+                    closed_indent -= self.spaces
+
+            outlines.append(line)
+
+        # remove that extra line we added
+        outlines.pop()
+
+        result = '\n'.join(outlines)
+        self.logger.debug(result)
+
+        return result
 
 
-def sass_to_scss(sass_lines, spaces=4):
-    outlines = []
-    cleaned = []
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Convert a Sass file to SCSS.')
+    parser.add_argument('file', type=argparse.FileType('r'))
+    parser.add_argument('--spaces', help="Number of spaces used for indentation", default=DEFAULT_INDENTATION)
+    parser.add_argument('--debug', help="Output debug information", dest='debug', action='store_true')
+    args = parser.parse_args()
 
-    re_css_rule = re.compile(r'[\w\-\d]+:')
-    re_import = re.compile(r'@import')
-    re_sass_mixin = re.compile(r'\=\w+')
-    re_sass_include = re.compile(r'\+\w+')
+    sassy = SassToSCSS(loglevel=args.debug or logging.INFO, spaces=args.spaces)
 
-    for line in sass_lines:
-        line = line.replace('\n', '')
-
-        if re_import.match(line):
-            line = line.replace('@import ', '@import "')
-            line += '";'
-
-        if re_sass_mixin.search(line):
-            line = line.replace('=', '@mixin ')
-
-        if re_sass_include.search(line):
-            line = line.replace('+', '@include ')
-            line += ';'
-
-        if re_css_rule.search(line):
-            line += ';'
-
-        cleaned.append(line)
-
-    # add an extra element in case we get to the end and
-    # we need one more iteration to figure out the }
-    # because we need to get next_indent
-    cleaned.append('')
-
-    for i, line in enumerate(cleaned):
-        indent = count_leading_spaces(cleaned[i])
-        try:
-            next_indent = count_leading_spaces(cleaned[i + 1])
-        except IndexError:
-            next_indent = 0
-        try:
-            prev_indent = count_leading_spaces(cleaned[i - 1])
-        except IndexError:
-            prev_indent = 0
-
-        print i, "next_indent", next_indent, "indent", indent, "prev_indent", prev_indent, line
-
-        if indent < next_indent:
-            line = line + ' ' + '{'
-            print '{'
-
-        if indent < prev_indent:
-            print i
-            closed_indent = prev_indent
-            while closed_indent >= next_indent and closed_indent != 0:
-                print '} : ', closed_indent, '- ', next_indent, '=', closed_indent - next_indent
-                insert = ' ' * (closed_indent - spaces) + '}'
-                outlines.append(insert)
-                closed_indent -= spaces
-
-        outlines.append(line)
-
-    # remove that extra line we added
-    outlines.pop()
-    return '\n'.join(outlines)
+    lines = args.file.readlines()
+    print sassy.convert(lines)
--- test_sass_to_scss.py
+++ test_sass_to_scss.py
@@ -1,19 +1,48 @@
+import logging
 from unittest import TestCase
-
-import sass_to_scss
+from sass_to_scss import SassToSCSS
 
 
 class TestSassToSCSS(TestCase):
 
+    def setUp(self):
+        self.sassy = SassToSCSS(loglevel=logging.DEBUG)
+
     def trim_docstring(self, docstring):
         return '\n'.join(docstring.split('\n')[1:-1])
+
+    def test_double_spaces(self):
+        self.sassy.spaces = 2
+        sass = """
+body
+  background: red
+  header
+    background: blue
+"""
+        scss = """
+body {
+  background: red;
+  header {
+    background: blue;
+  }
+}
+"""
+        actual = self.sassy.convert(self.trim_docstring(sass).split('\n'))
+        expected = self.trim_docstring(scss)
+
+        self.assertEquals(expected, actual)
 
     def test_triple_nested_class(self):
         sass = """
 @import bourbon/bourbon
+@import "http://fonts.googleapis.com/css?family=PT+Serif:400,700,400italic,700italic|Oswald:400,300,700|Droid+Sans:400,700"
+
+$teal: #00917d
+$serif: 'PT Serif', serif
 
 =red-text
     color: red
+    background-color: $teal
 
 =rounded($amount, $background_color)
     -moz-border-radius: $amount
@@ -32,7 +61,8 @@ class TestSassToSCSS(TestCase):
     top: 0
     color: green
     bottom: 0
-    .right
+
+    .right, .row:hover, &.touch .row
         right: 0
         border: 1px solid #fff
         h1
@@ -45,18 +75,19 @@ class TestSassToSCSS(TestCase):
 
         scss = """
 @import "bourbon/bourbon";
-
+@import "http://fonts.googleapis.com/css?family=PT+Serif:400,700,400italic,700italic|Oswald:400,300,700|Droid+Sans:400,700";
+$teal: #00917d;
+$serif: 'PT Serif', serif;
 @mixin red-text {
     color: red;
+    background-color: $teal;
 }
-
 @mixin rounded($amount, $background_color) {
     -moz-border-radius: $amount;
     -webkit-border-radius: $amount;
     border-radius: $amount;
     background-color: saturate(lighten($background_color, 30%), 100%);
 }
-
 .error {
     @include red-text;
     .details {
@@ -64,13 +95,12 @@ class TestSassToSCSS(TestCase):
         @include rounded(0.5em, desaturate(#5336a2, 10%));
     }
 }
-
 .container {
     position: absolute;
     top: 0;
     color: green;
     bottom: 0;
-    .right {
+    .right, .row:hover, &.touch .row {
         right: 0;
         border: 1px solid #fff;
         h1 {
@@ -86,9 +116,7 @@ class TestSassToSCSS(TestCase):
 }
         """
 
-        input = self.trim_docstring(sass).split('\n')
-        actual = sass_to_scss.sass_to_scss(input)
+        actual = self.sassy.convert(self.trim_docstring(sass).split('\n'))
         expected = self.trim_docstring(scss)
-        print actual
 
         self.assertEquals(expected, actual)
